7ee3ad585c4000a095c9f94840ae93d7cf23ace5
jeanjoellevillanuevaspectrumone <javillanueva@spectrumone.co>
2019-12-19 18:56:58 +0800

    i

---
+++ populate_rango.py
@@ -0,0 +1,62 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django_project.settings')
+
+import django
+django.setup()
+from rango.models import Category, Page
+
+def populate():
+
+    python_pages = [
+        {'title': 'Official Python Tutorial',
+         'url': 'http://docs.python.org/3/tutorial/'},
+        {'title': 'How to Think like a computer scientist',
+         'url': 'http://www.greenteapress.com/thinkpython/'},
+        {'title': 'Learn Python in 10 minutes',
+         'url': 'http://www.korokithakis.net/tutorials/python'}]
+
+    django_pages = [
+        {'title': 'Official Django Tutorial',
+         'url': 'http://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        {'title': 'Django rocks',
+         'url': 'http://www.djangorokcs.com/'},
+        {'title': 'How to Tango with Django',
+         'url': 'http://www.Tangowithdjango.com/'}]
+
+    other_pages = [
+            {'title': 'Bottle',
+             'url':'http://bottlepy.org/docs/dev/'},
+            {'title': 'Flask',
+             'url': 'http://flask.pocoo.org'}]
+
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
+
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
+        for p in cat_data['pages']:
+            add_page(c, p['title'], p['url'])
+
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print('- {0} - {1}'.format(str(c), str(p)))
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.name=name
+    c.views=views
+    c.likes=likes
+    c.save()
+    return c
+
+if __name__ == '__main__':
+    print('Starting Rango population script...')
+    populate()
